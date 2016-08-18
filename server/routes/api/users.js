080770817137449fbb1a710d847653be22bf0643
Leonard Lausen <leonard@lausen.nl>
2016-08-18 21:25:03 +1000

    server: extend user api
    Allow retrieval of number of comments / reports of a user

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var config = require('../../config');
+var Report = require('../../models/report');
+var Comment = require('../../models/comment');
 var ManagementClient = require('auth0').ManagementClient;
 
 var management = new ManagementClient({
@@ -10,13 +12,32 @@ var management = new ManagementClient({
 
 // Get users
 router.get('/:user_id', function(req, res, next) {
-  console.log(req.params);
   management.users.get({"id": req.params.user_id})
     .then((user) => {
       return res.json({
         "name": user.name,
         "nickname": user.nickname
       });
+    })
+    .catch(next);
+});
+
+// Count of reports per user
+router.get('/:user_id/reports/count', function(req, res, next) {
+  Report.count(req.query)
+    .exec()
+    .then((count) => {
+      return res.json(count);
+    })
+    .catch(next);
+});
+
+// Count of comments per user
+router.get('/:user_id/comments/count', function(req, res, next) {
+  Comment.count(req.query)
+    .exec()
+    .then((count) => {
+      return res.json(count);
     })
     .catch(next);
 });
